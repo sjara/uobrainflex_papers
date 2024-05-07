e8229b9302bbd9e965b06e5f91aa7ec79045af1b
sjara <sjara@uoregon.edu>
2024-05-07 10:56:23 -0700

    Added script showing how to read the NWB files.

---
+++ papadopoulos2024/example_read_nwb.py
@@ -0,0 +1,36 @@
+"""
+Example showing how to read an NWB file created by 'convert_ephys_to_nwb.py'
+
+Usage:
+python example_read_nwb.py ./LA3_session3_processed_data.nwb
+"""
+
+import pynwb
+import sys
+
+if len(sys.argv) > 1:
+    nwb_path = sys.argv[1]
+else:
+    raise ValueError('Please provide an NWB file to load.')
+
+ioObj = pynwb.NWBHDF5IO(nwb_path, 'r', load_namespaces=True)
+nwbfile = ioObj.read()
+
+input('Press ENTER to show stimulus data...')
+print('--- Stimulus data (as a dataframe) ---')
+print(nwbfile.trials.to_dataframe())
+
+input('Press ENTER to show spike times...')
+print('--- Spike times (as a dataframe) ---')
+print(nwbfile.units.to_dataframe())
+
+input('Press ENTER to show pupil data...')
+print('--- Pupil diameter (data and timestamps)---')
+print(nwbfile.processing['behavior']['PupilTracking']['pupil_diameter'].data[()])
+print(nwbfile.processing['behavior']['PupilTracking']['pupil_diameter'].timestamps[()])
+
+input('Press ENTER to show running data...')
+print('--- Running speed (data and timestamps)---')
+print(nwbfile.processing['behavior']['running_speed'].data[()])
+print(nwbfile.processing['behavior']['running_speed'].timestamps[()])
+
